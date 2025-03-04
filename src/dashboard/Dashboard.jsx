3a38544fe9160b6351480fad3e04051e614198fc
Alex <alexdusselaar@gmail.com>
2025-03-04 15:46:51 +0100

    start dash layout

--- src/dashboard/Dashboard.jsx
+++ src/dashboard/Dashboard.jsx
@@ -1,8 +1,17 @@
+import {Link} from "react-router";
+
 function Dashboard() {
     return (
         <>
-            <section>
-                <p>Dashboard</p>
+            <section class="flex flex-row m-10">
+                <article className="basis-1/2 rounded-md border-black border-2">
+                    <h3>Les 1</h3>
+                    <a>Start les</a>
+                </article>
+                <article className="basis-1/2">
+                    <h3>Les 2</h3>
+                    <a>Start les</a>
+                </article>
             </section>
         </>
     )

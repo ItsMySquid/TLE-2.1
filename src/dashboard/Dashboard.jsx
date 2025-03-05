55922e5d16649f3820d06a1ceb4c8d0b68d11dd1
Alex <alexdusselaar@gmail.com>
2025-03-05 14:28:43 +0100

    small dash css fix

--- src/dashboard/Dashboard.jsx
+++ src/dashboard/Dashboard.jsx
@@ -1,9 +1,12 @@
 import {Link} from "react-router";
 
 function Dashboard() {
+    
     return (
         <>
             <section className="m-8">
+                {/*ZOEKBALK --> to be added because it already exists just not on this page */}
+
                 {/* CURSUSVOORTGANG */}
                 <section className="m-12">
                     <div className="flex justify-between">
@@ -14,23 +17,43 @@ function Dashboard() {
                 </section>
                 {/* LESSEN OVERZICHT */}
                 <section className="flex flex-row flex-wrap justify-evenly">
-                    <article className="basis-[40vw] rounded-md border-[#354B46] border-2 p-4 m-4">
+                    <article className="basis-[43vw] rounded-md border-[#354B46] border-2 p-4 m-4">
                         <h2 className="text-2xl mb-4 text-[#354B46]">Les 1</h2>
                         <a className="justify-center flex bg-[#354B46] text-[#E8F1F5] px-4 py-2 rounded-md hover:bg-[#2D8474] transition duration-200 ease-in-out"
                            href="#">Start les</a>
                     </article>
-                    <article className="basis-[40vw] rounded-md border-[#354B46] border-2 p-4 m-4">
+                    <article className="basis-[43vw] rounded-md border-[#354B46] border-2 p-4 m-4">
                         <h2 className="text-2xl mb-4 text-[#354B46]">Les 2</h2>
                         <a className="justify-center flex bg-[#354B46] text-[#E8F1F5] px-4 py-2 rounded-md hover:bg-[#2D8474] transition duration-200 ease-in-out"
                            href="#">Start les</a>
                     </article>
-                    <article className="basis-[40vw] rounded-md border-[#354B46] border-2 p-4 m-4">
+                    <article className="basis-[43vw] rounded-md border-[#354B46] border-2 p-4 m-4">
                         <h2 className="text-2xl mb-4 text-[#354B46]">Les 3</h2>
                         <a className="justify-center flex bg-[#354B46] text-[#E8F1F5] px-4 py-2 rounded-md hover:bg-[#2D8474] transition duration-200 ease-in-out"
                            href="#">Start les</a>
                     </article>
-                    <article className="basis-[40vw] rounded-md border-[#354B46] border-2 p-4 m-4">
+                    <article className="basis-[43vw] rounded-md border-[#354B46] border-2 p-4 m-4">
                         <h2 className="text-2xl mb-4 text-[#354B46]">Les 4</h2>
+                        <a className="justify-center flex bg-[#354B46] text-[#E8F1F5] px-4 py-2 rounded-md hover:bg-[#2D8474] transition duration-200 ease-in-out"
+                           href="#">Start les</a>
+                    </article>
+                    <article className="basis-[43vw] rounded-md border-[#354B46] border-2 p-4 m-4">
+                        <h2 className="text-2xl mb-4 text-[#354B46]">Les 5</h2>
+                        <a className="justify-center flex bg-[#354B46] text-[#E8F1F5] px-4 py-2 rounded-md hover:bg-[#2D8474] transition duration-200 ease-in-out"
+                           href="#">Start les</a>
+                    </article>
+                    <article className="basis-[43vw] rounded-md border-[#354B46] border-2 p-4 m-4">
+                        <h2 className="text-2xl mb-4 text-[#354B46]">Les 6</h2>
+                        <a className="justify-center flex bg-[#354B46] text-[#E8F1F5] px-4 py-2 rounded-md hover:bg-[#2D8474] transition duration-200 ease-in-out"
+                           href="#">Start les</a>
+                    </article>
+                    <article className="basis-[43vw] rounded-md border-[#354B46] border-2 p-4 m-4">
+                        <h2 className="text-2xl mb-4 text-[#354B46]">Les 7</h2>
+                        <a className="justify-center flex bg-[#354B46] text-[#E8F1F5] px-4 py-2 rounded-md hover:bg-[#2D8474] transition duration-200 ease-in-out"
+                           href="#">Start les</a>
+                    </article>
+                    <article className="basis-[43vw] rounded-md border-[#354B46] border-2 p-4 m-4">
+                        <h2 className="text-2xl mb-4 text-[#354B46]">Les 8</h2>
                         <a className="justify-center flex bg-[#354B46] text-[#E8F1F5] px-4 py-2 rounded-md hover:bg-[#2D8474] transition duration-200 ease-in-out"
                            href="#">Start les</a>
                     </article>

9b28a0fdfaf7676131d3e39d954b09d53ed3fcea
Alex <alexdusselaar@gmail.com>
2025-03-10 15:32:55 +0100

    dash colors changed to theme

--- src/dashboard/Dashboard.jsx
+++ src/dashboard/Dashboard.jsx
@@ -19,31 +19,31 @@ function Dashboard() {
                         />
                     </div>
                     <div className="flex justify-between">
-                        <h2 className="text-xl mb-4 text-[#354B46]">Cursusvoortgang</h2>
-                        <p>0/8</p>
+                        <h2 className="text-xl mb-4 text-tekstColor-100">Cursusvoortgang</h2>
+                        <p className="text-tekstColor-100">0/8</p>
                     </div>
-                    <div className="h-2 flex bg-[#E8F1F5] rounded-md"></div>
+                    <div className="h-2 flex bg-backgroundColor-100 rounded-md"></div>
                 </section>
                 {/* LESSEN OVERZICHT */}
                 <section className="flex flex-row flex-wrap justify-evenly">
                     {lessen.map((les) =>
-                        <article className="basis-[43vw] rounded-md border-[#354B46] border-2 p-4 m-4">
+                        <article className="basis-[43vw] rounded-md border-tekstColor-100 border-2 p-4 m-4">
                             <div className="flex flex-row justify-between">
-                                <h2 className="text-2xl mb-4 text-[#354B46]">{les}</h2>
-                                <h2 className="text-[#354B46]">0/20</h2>
+                                <h2 className="text-2xl mb-4 text-tekstColor-100">{les}</h2>
+                                <h2 className="text-tekstColor-100">0/20</h2>
                             </div>
-                            <a className="justify-center flex bg-[#354B46] text-[#E8F1F5] px-4 py-2 rounded-md hover:bg-[#2D8474] transition duration-200 ease-in-out"
-                               href="#">Start les</a>
+                            <a className="justify-center flex bg-tekstColor-100 text-backgroundColor-100 px-4 py-2 rounded-md hover:bg-borderColor-100 transition duration-200 ease-in-out"
+                               href={les}>Start les</a>
                         </article>
                     )}
                 </section>
 
-                    {/*<article className="basis-[43vw] rounded-md border-[#354B46] border-2 p-4 m-4">*/}
+                    {/*<article className="basis-[43vw] rounded-md border-tekstcolor-100 border-2 p-4 m-4">*/}
                     {/*    <div className="flex flex-row justify-between">*/}
-                    {/*        <h2 className="text-2xl mb-4 text-[#354B46]">Les 1</h2>*/}
-                    {/*        <h2 className="text-[#354B46]">20/20</h2>*/}
+                    {/*        <h2 className="text-2xl mb-4 text-tekstcolor-100">Les 1</h2>*/}
+                    {/*        <h2 className="text-tekstcolor-100">20/20</h2>*/}
                     {/*    </div>*/}
-                    {/*    <a className="border-[#354B46] border-2 justify-center flex bg-[#E8F1F5] text-[#354B46] px-4 py-2 rounded-md hover:bg-[#2D8474] transition duration-200 ease-in-out"*/}
+                    {/*    <a className="border-tekstcolor-100 border-2 justify-center flex bg-backgroundColor-100 text-tekstcolor-100 px-4 py-2 rounded-md hover:bg-bordercolor-100 transition duration-200 ease-in-out"*/}
                     {/*           href="#">Opnieuw oefenen</a>*/}
                     {/*    </article>*/}
                 </section>

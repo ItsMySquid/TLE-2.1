e7dddd94ed736c39a3faee0f047b18de4f079ebd
Dirk van Gelderen <94358540+ItsMySquid@users.noreply.github.com>
2025-03-13 12:58:11 +0100

    Small Bugfix

--- src/assets/Layout.jsx
+++ src/assets/Layout.jsx
@@ -7,14 +7,17 @@ function Layout() {
             <header className="bg-headerColor-100 text-white py-10 px-28 flex items-center">
                 {/* Container voor logo en tekst, verder naar links */}
                 <div className="flex items-center mr-auto">
-                    <img src="/logo.svg" alt="Logo" className="w-20 h-20 mr-3" />
-                    <h1 className="text-xl font-bold text-tekstColor-100">Gebarentaal</h1>
+                    <Link to="./" className="flex items-center">
+                        <img src="/logo.svg" alt="Logo" className="w-20 h-20 mr-3"/>
+                        <h1 className="text-xl font-bold text-tekstColor-100">Gebarentaal</h1>
+                    </Link>
                 </div>
+
 
                 {/* Centraal geplaatste navigatie links */}
                 <div className="flex space-x-6 absolute left-1/2 transform -translate-x-1/2">
                     <Link to={`/woordenboek`} className="text-white hover:text-green-600">
-                        Woordenboek
+                    Woordenboek
                     </Link>
                     <Link to={`/les`} className="text-white hover:text-green-600">
                         Lessen

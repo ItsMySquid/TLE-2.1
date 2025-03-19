e73f2490ed879704c2a697ced743f75977bf08a0
Alex <alexdusselaar@gmail.com>
2025-03-19 13:49:06 +0100

    protectedroute turned back on

--- src/assets/App.jsx
+++ src/assets/App.jsx
@@ -17,7 +17,7 @@ const router = createBrowserRouter([
     { path: "/login", element: <Login /> }, // Tussenpagina
 
     {
-        //element: <ProtectedRoute />, // Beveiligde routes
+        element: <ProtectedRoute />, // Beveiligde routes
         children: [
             {
                 element: <Layout />,
